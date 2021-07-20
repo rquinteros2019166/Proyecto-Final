e44a18b3456d0abec69707ea073a1855531b50bd
Pedro <pteo-2019183@kinal.edu.gt>
2021-07-20 15:19:39 -0600

    Se creo el login y el register

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: 'navbar', component: NavbarComponent }
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'login' , component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
