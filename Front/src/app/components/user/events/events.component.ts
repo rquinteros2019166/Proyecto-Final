1f73be73af8a0b1fa4f0a67f73e420ed6315c437
Pedro <pteo-2019183@kinal.edu.gt>
2021-08-02 00:24:18 -0600

    Se le dio funcionalidad a events de parte del cliente

--- Front/src/app/components/user/events/events.component.ts
+++ Front/src/app/components/user/events/events.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Events } from 'src/app/models/eventos.models';
+import { EventService } from 'src/app/service/event.service';
 import { UserService } from 'src/app/service/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-events',
@@ -13,6 +15,7 @@ export class EventsComponent implements OnInit {
   public modelEvent: Events;
 
   constructor(
+    public  _eventService: EventService,
     public _userService: UserService,
     public _router: Router
   ) {
@@ -25,5 +28,43 @@ export class EventsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+addEvent(){
+  this._eventService.registerEvents(this.modelEvent, this.token)
+  .subscribe(
+      response=>{
+        console.log(response)
+
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Se creo correctamente el evento',
+            showConfirmButton: false,
+            timer: 1500
+        })
+        this._router.navigate(['/home'])
+      },
+      error=>{
+        console.log(<any>error)
+
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: error.error.message,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+  )
+}
+
+limpiar(){
+  this.modelEvent = {
+   nameEvent: "",
+   descriptionEvent: "",
+   statusEvent: "",
+   typeEvent: "",
+   dateEvent: new Date(Date.now()).toISOString().substr(0, 16)
+  };
+}
 
 }
