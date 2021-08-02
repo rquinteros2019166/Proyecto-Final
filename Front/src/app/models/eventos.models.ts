126b1ab465f41738331a532cd9dcd8d210303b58
fmontiel2019145 <fmontiel-2019145@kinal.edu.gt>
2021-08-02 12:06:22 -0600

    arreglado el modelo eventos

--- Front/src/app/models/eventos.models.ts
+++ Front/src/app/models/eventos.models.ts
@@ -1,10 +1,11 @@
 export class Events{
   constructor(
+    public _id: String,
     public nameEvent: String,
     public descriptionEvent: String,
     public statusEvent: String,
     public typeEvent: String,
-    public dateEvent: any,
+    public dateEvent: any
 
   ){}
 }
